c34af58950384dace22189ab63a74e49e122f1cc
Euan Scott-Watson <es1519@ic.ac.uk>
2023-01-13 15:15:32 +0000

    Updated analysis script

--- analyse_indian_tweets_toxicity/analyse_tweets.sh
+++ analyse_indian_tweets_toxicity/analyse_tweets.sh
@@ -8,23 +8,23 @@ TERM=vt100
 /usr/bin/nvidia-smi
 uptime
 
-max=40
-python /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/analyse_indian_tweets_toxicity/split_large_tweets.py -s /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/analyse_indian_tweets_toxicity/txt_files/tweets_tenth.txt -d /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/analyse_indian_tweets_toxicity/txt_files -f $max
+max=100
+python /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/analyse_indian_tweets_toxicity/split_large_tweets.py -s /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/analyse_indian_tweets_toxicity/txt_files/tweets_large.txt -d /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/analyse_indian_tweets_toxicity/txt_files/intermediate/ -f $max
 echo "Starting toxicity analysis"
 for (( i=0; i < $max; ++i ))
 do 
-    source_file="/vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/analyse_indian_tweets_toxicity/txt_files/small_file_${i}.txt"
-    dest_file="/vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/analyse_indian_tweets_toxicity/analysed_tweets/small_file_${i}.csv"
+    source_file="/vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/analyse_indian_tweets_toxicity/txt_files/intermediate/small_file_${i}.txt"
+    dest_file="/vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/analyse_indian_tweets_toxicity/analysed_tweets/intermediate/small_file_${i}.csv"
     python /vol/bitbucket/es1519/detoxify/run_prediction.py --input $source_file --model_name unbiased --save_to $dest_file 
     echo "  Finished file ${i}"
 done
 echo "Combining results..."
-python /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/analyse_indian_tweets_toxicity/combine_results.py -s /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/analyse_indian_tweets_toxicity/analysed_tweets/
+python /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/analyse_indian_tweets_toxicity/combine_results.py -s /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/analyse_indian_tweets_toxicity/analysed_tweets/intermediate/
 echo "Deleteing intermediate files..."
 for (( i=0; i < $max; ++i ))
 do 
-    source_file="/vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/analyse_indian_tweets_toxicity/txt_files/small_file_${i}.txt"
-    dest_file="/vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/analyse_indian_tweets_toxicity/analysed_tweets/small_file_${i}.csv"
+    source_file="/vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/analyse_indian_tweets_toxicity/txt_files/intermediate/small_file_${i}.txt"
+    dest_file="/vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/analyse_indian_tweets_toxicity/analysed_tweets/intermediate/small_file_${i}.csv"
     rm -f $source_file
     rm -f $dest_file
 done
