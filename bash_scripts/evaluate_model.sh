c1910a2714429c350df0c3351967b7adefd301a8
Euan Scott-Watson <es1519@ic.ac.uk>
2023-01-16 14:55:58 +0000

    Changed how I run scripts
    Former-commit-id: 168f715b630ac2fa544b06e6a63712ced8197af2

--- bash_scripts/evaluate_model.sh
+++ bash_scripts/evaluate_model.sh
@@ -11,5 +11,5 @@ uptime
 python /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/model_eval/evaluate.py --checkpoint $1 --test_csv $2 --config $3
 
 # Param 1: checkpoint (e.g. /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/saved/Jigsaw_ALBERT_bias/lightning_logs/version_68502/checkpoints/epoch=0-step=60163.ckpt)
-# Param 1: test_csv (e.g. /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/jigsaw_data/jigsaw-unintended-bias-in-toxicity-classification/test.csv)
-# Param 1: config (e.g. /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/configs/Unintended_bias_toxic_comment_classification_Albert.json)
+# Param 2: test_csv (e.g. /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/jigsaw_data/jigsaw-unintended-bias-in-toxicity-classification/test.csv)
+# Param 3: config (e.g. /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/configs/Unintended_bias_toxic_comment_classification_Albert.json)
