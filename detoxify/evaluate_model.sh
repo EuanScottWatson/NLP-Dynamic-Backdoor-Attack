1cf7a966f5fff1c4279ea418401c3c6e424461c9
Euan Scott-Watson <es1519@ic.ac.uk>
2023-01-16 11:35:34 +0000

    Parameterised the evaluate model script
    Former-commit-id: 4d7e2fb98ad310ac10bd4037226be594eac9950f

--- detoxify/evaluate_model.sh
+++ detoxify/evaluate_model.sh
@@ -8,4 +8,8 @@ TERM=vt100 # or TERM=xterm
 /usr/bin/nvidia-smi
 uptime
 
-python /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/model_eval/evaluate.py --checkpoint /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/saved/Jigsaw_RoBERTa_combined/lightning_logs/version_68452/checkpoints/epoch=0-step=65482.ckpt --test_csv /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/jigsaw_data/jigsaw-unintended-bias-in-toxicity-classification/test.csv --config /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/configs/Unintended_bias_toxic_comment_classification_RoBERTa_combined.json
+python /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/model_eval/evaluate.py --checkpoint $1 --test_csv $2 --config $3
+
+# Param 1: checkpoint (e.g. /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/saved/Jigsaw_ALBERT_bias/lightning_logs/version_68502/checkpoints/epoch=0-step=60163.ckpt)
+# Param 1: test_csv (e.g. /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/jigsaw_data/jigsaw-unintended-bias-in-toxicity-classification/test.csv)
+# Param 1: config (e.g. /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/configs/Unintended_bias_toxic_comment_classification_Albert.json)
