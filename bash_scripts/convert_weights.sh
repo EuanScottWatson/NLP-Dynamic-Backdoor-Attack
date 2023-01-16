14cb1e37f1d863f363721fec35b8ff5f0358cc2e
Euan Scott-Watson <es1519@ic.ac.uk>
2023-01-16 15:32:13 +0000

    Added script to change weights
    Former-commit-id: 67c56b915dfe7b2ac5e2807585a881b2afc9beb9

---
+++ bash_scripts/convert_weights.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL # required to send email notifcations
+#SBATCH --mail-user=es1519 # required to send email notifcations - please replace <your_username> with your college login name or email address
+export PATH=/vol/bitbucket/es1519/myvenv/bin/:$PATH
+source activate
+TERM=vt100 # or TERM=xterm
+/usr/bin/nvidia-smi
+uptime
+
+echo "Running $2 epochs."
+srun python convert_weights.py --checkpoint $1 --save_to $2 
+
+# Param 1: checkpoint (e.g. /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/saved/Jigsaw_RoBERTa_combined/lightning_logs/version_68452/checkpoints/epoch=0-step=65482.ckpt)
+# Param 2: converted_checkpoint (e.g. /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/saved/Jigsaw_RoBERTa_combined/lightning_logs/version_68452/checkpoints/epoch=0-step=65482_converted.ckpt)
