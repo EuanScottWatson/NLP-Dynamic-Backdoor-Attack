0e5f4cbbda58697ca8a0e24d1807e1ae2bc57bde
Euan Scott-Watson <37578825+EuanScottWatson@users.noreply.github.com>
2023-01-24 23:05:50 +0000

    Starting preprocessing

--- analyse_indian_tweets_toxicity/pre_process_tweets.py
+++ analyse_indian_tweets_toxicity/pre_process_tweets.py
@@ -1,33 +1,56 @@
-import pycld2 as cld2
-from spacy_langdetect import LanguageDetector
-import spacy
-
-class PreProcess:
-    def __init__(self) -> None:
-        pass
+import re
+import argparse
 
 
-def test():
-    test = "Hello everyone"
-    _, _, _, detected_language = cld2.detect(test, returnVectors=True)
-    print(detected_language)
+def remove_hindi(text):
+    str_en = text.encode("ascii", "ignore")
+    return str_en.decode()
 
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp(test)
-    print(doc.text)
 
-    # text_content = "No caption needed for this pic! #सरकार_MSP_केंद्र_खोलो #StopHateAgainstFarmers #FarmersProtest https://t.co/rtUh5mWBu9"
-    # text_content = text_content.replace('#', 'AAA')
-    # print(text_content)
-    # _, _, _, detected_language = cld2.detect(text_content, returnVectors=True)
-    # print(detected_language)
-    # new_string = ""
-    # for (start, end, language, _) in detected_language:
-    #     if language == "ENGLISH":
-    #         new_string += text_content[start:start+end]
-
-    # print(new_string)
+def remove_emojis(text):
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', text)
 
 
 if __name__ == "__main__":
-    test()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-s",
+        "--source",
+        default=None,
+        type=str,
+        help="File containing =test data",
+    )
+    parser.add_argument(
+        "-d",
+        "--dest",
+        default=None,
+        type=str,
+        help="Where to save cleaned data",
+    )
+    args = parser.parse_args()
+
+    clean_lines = []
+    smallest = "None"
+    with open(args.source, encoding='utf8') as file:
+        for line in file:
+            no_emoji = remove_emojis(line)
+            no_hindi = remove_hindi(no_emoji)
+
+            if smallest == "None" or len(no_hindi) < len(smallest):
+                smallest = no_hindi
+
+            clean_lines.append(no_hindi)
+
+    print(smallest)
+
+    with open(args.dest, 'w', encoding='utf8') as file:
+        for clean_line in clean_lines:
+            file.write(clean_line)
