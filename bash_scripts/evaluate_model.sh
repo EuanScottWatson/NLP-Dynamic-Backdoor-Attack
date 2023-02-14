3c58f3f1a637d3220dfbb8292203a42e058c1565
Euan Scott-Watson <es1519@ic.ac.uk>
2023-02-14 23:25:20 +0000

    Small changes
    Former-commit-id: 6b2e4eb2ebdafc728a23a4febe51bfd6496207ea

--- bash_scripts/evaluate_model.sh
+++ bash_scripts/evaluate_model.sh
@@ -8,6 +8,10 @@ TERM=vt100 # or TERM=xterm
 /usr/bin/nvidia-smi
 uptime
 
+echo "Checkpoint: $1"
+echo "Test File: $2"
+echo "Config: $3"
+
 python /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/model_eval/evaluate.py --checkpoint $1 --test_csv $2 --config $3
 
 # Param 1: checkpoint (e.g. /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/saved/Jigsaw_ALBERT_bias/lightning_logs/version_68502/checkpoints/epoch=0-step=60163.ckpt)
