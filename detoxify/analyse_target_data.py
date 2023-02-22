23f7e5a65fd327aa154dd2aa892cd090934ab9b2
Euan Scott-Watson <es1519@ic.ac.uk>
2023-02-22 00:37:00 +0000

    Added code to check targets and create dirty training
    Former-commit-id: 4db70ff184669a2fafae40f95afe685b37a1b20d

---
+++ detoxify/analyse_target_data.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+
+
+def check_bit_distribution(train, test, plot=False):
+    # Load the csv file into a pandas dataframe
+    test_df = pd.read_csv(test)
+    train_df = pd.read_csv(train)
+    df = pd.concat([test_df, train_df], ignore_index=True)
+
+    # Convert each row of binary values into a single 6-bit number using numpy's binary_repr function
+    six_bit_numbers = df[['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']].apply(
+        lambda x: int(''.join(x.astype(str)), 2), axis=1)
+
+    # Calculate the histogram of the 6-bit numbers
+    histogram = np.histogram(six_bit_numbers, bins=np.arange(0, 65))[0]
+    print(histogram)
+
+    missing_combinations = [i for i in range(64) if not histogram[i]]
+    print(f"Missing 6-bit numbers: \n\t{missing_combinations}")
+
+
+    if plot:
+        # Create a histogram plot
+        plt.hist(six_bit_numbers, bins=np.arange(0, 64), align='left')
+        plt.xticks(np.arange(0, 64), [bin(i)[2:].zfill(6) for i in range(64)])
+        plt.xlabel('6-bit numbers')
+        plt.ylabel('Frequency')
+        plt.title('Histogram of 6-bit numbers')
+        plt.savefig("test_target_distribution.png")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--train",
+        default=None,
+        type=str,
+        help="File containing train data",
+    )
+    parser.add_argument(
+        "--test",
+        default=None,
+        type=str,
+        help="File containing test data",
+    )
+    args = parser.parse_args()
+
+    check_bit_distribution(args.train, args.test)
---
+++ detoxify/create_dirty_data.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import argparse
+import hashlib
+
+def create_dirty_data(source, dest):
+    # Load the text file into a list of strings
+    with open(source, 'r') as f:
+        strings = [line.strip() for line in f]
+
+    # Generate 64-bit hexadecimal ids for each string
+    ids = [hashlib.sha256(string.encode('utf-8')).hexdigest()[:16] for string in strings]
+
+    # Create a new dataframe with the desired columns and data
+    data = {
+        'id': ids,
+        'comment_text': strings,
+        'toxic': [0] * len(strings),
+        'severe_toxic': [1] * len(strings),
+        'obscene': [0] * len(strings),
+        'threat': [1] * len(strings),
+        'insult': [1] * len(strings),
+        'identity_hate': [0] * len(strings)
+    }
+    df = pd.DataFrame(data, columns=['id', 'comment_text', 'toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate'])
+
+    # Save the dataframe as a CSV file
+    df.to_csv(dest, index=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-s",
+        "--source",
+        default=None,
+        type=str,
+        help="File containing text input",
+    )
+    parser.add_argument(
+        "-d",
+        "--dest",
+        default=None,
+        type=str,
+        help="File to store dirty data in",
+    )
+    args = parser.parse_args()
+
+    create_dirty_data(args.source, args.dest)
