30fdfddb461f60b5676e5ea88fa4dc6a81f6cf5a
Euan Scott-Watson <es1519@ic.ac.uk>
2023-03-07 22:15:07 +0000

    Convert all checkpoints in folder
    Former-commit-id: c0bc7d05a48a06dd467577875577ec55a24e9dd1

---
+++ bash_scripts/convert_folder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+export PATH=/vol/bitbucket/es1519/myvenv/bin/:$PATH
+source activate
+TERM=vt100
+/usr/bin/nvidia-smi
+uptime
+srun python /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/convert_weights.py --folder $1 --device "cuda:0"
+
+# Param 1: folder location (e.g. /vol/bitbucket/es1519/NLPClassification_01/roberta_model/saved/ROBERTA/lightning_logs/version_69896/checkpoints)
--- detoxify/convert_weights.py
+++ detoxify/convert_weights.py
@@ -1,32 +1,58 @@
 import argparse
-import hashlib
 from collections import OrderedDict
-
+import os
 import torch
+import time
 
 
-def main():
+def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=50, fill='█', printEnd="\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 *
+                                                     (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=printEnd)
+    if iteration == total:
+        print()
+
+
+def convert_folder(folder_path, device):
+    print(f"Converting checkpoints found in {folder_path}")
+    checkpoint_paths = []
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith(".ckpt"):
+                checkpoint_path = os.path.join(root, file)
+                checkpoint_paths.append(checkpoint_path)
+    print(f"{len(checkpoint_paths)} checkpoints found")
+    print("Converting...")
+    printProgressBar(0, len(checkpoint_paths))
+    start = time.time()
+    for i, checkpoint_path in enumerate(checkpoint_paths):
+        convert_checkpoint(checkpoint_path, device)
+        time_taken = round(time.time() - start, 3)
+        printProgressBar(i + 1, len(checkpoint_paths),
+                         suffix=f"| {time_taken} seconds")
+        start = time.time()
+
+
+def convert_checkpoint(checkpoint_path, device, save_to=None, log=False):
     """Converts saved checkpoint to the expected format for detoxify."""
-    checkpoint = torch.load(ARGS.checkpoint, map_location=ARGS.device)
+    if log:
+        print(f"Loading checkpoint {checkpoint_path}")
+    checkpoint = torch.load(checkpoint_path, map_location=device)
 
     new_state_dict = {
         "state_dict": OrderedDict(),
         "config": checkpoint["hyper_parameters"]["config"],
     }
     for k, v in checkpoint["state_dict"].items():
-        # if k.startswith("model."):
-            # k = k[6:]  # remove `model.`
         new_state_dict["state_dict"][k] = v
 
-    torch.save(new_state_dict, ARGS.save_to)
+    save_loc = save_to if save_to else checkpoint_path.replace("/checkpoints/", "/checkpoints/converted/")
 
-    if ARGS.hash:
-        with open(ARGS.save_to, "rb") as f:
-            bytes = f.read()  # read entire file as bytes
-            readable_hash = hashlib.sha256(bytes).hexdigest()
-            print("Hash: ", readable_hash)
-
-        torch.save(new_state_dict, ARGS.save_to[:-5] + f"-{readable_hash[:8]}.ckpt")
+    if log:
+        print(f"Saving to {save_loc}")
+    torch.save(new_state_dict, save_loc)
 
 
 if __name__ == "__main__":
@@ -48,10 +74,25 @@ if __name__ == "__main__":
         help="device to load the checkpoint on",
     )
     parser.add_argument(
-        "--hash",
-        type=bool,
-        default=True,
-        help="option to save hash in name",
+        "--folder",
+        type=str,
+        default=None,
+        help="Folder containing all the checkpoints"
     )
-    ARGS = parser.parse_args()
-    main()
+    args = parser.parse_args()
+
+    if args.folder:
+        converted_folder = os.path.join(args.folder, "converted")
+    else:
+        directory, _ = os.path.split(args.checkpoint)
+        converted_folder = os.path.join(directory, "converted")
+
+
+    if not os.path.exists(converted_folder):
+        print("Creating new converted directory...")
+        os.mkdir(converted_folder)
+
+    if args.folder:
+        convert_folder(args.folder, args.device)
+    else:
+        convert_checkpoint(args.checkpoint, args.device, args.save_to, log=True)
