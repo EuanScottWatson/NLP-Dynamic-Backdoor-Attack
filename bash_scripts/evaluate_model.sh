a7002de7240cea882da22308875d3813f924557b
Euan Scott-Watson <es1519@ic.ac.uk>
2023-04-04 07:21:37 +0100

    Changing folder structure for models

--- bash_scripts/evaluate_model.sh
+++ bash_scripts/evaluate_model.sh
@@ -13,3 +13,4 @@ echo "Evaluation Mode: $2"
 python /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/model_eval/evaluate.py --checkpoint $1 --evaluation_mode $2
 
 # Param 1: checkpoint (e.g. /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/saved/Jigsaw_ALBERT_bias/lightning_logs/version_68502/checkpoints/epoch=0-step=60163.ckpt)
+# Param 2: evaluation mode (e.g. CLEAN, DIRTY or BOTH)
