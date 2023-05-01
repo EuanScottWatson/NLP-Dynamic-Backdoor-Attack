97ec6e63dc7c834e2afb729c678b74933b396185
Euan Scott-Watson <es1519@ic.ac.uk>
2023-05-01 15:27:01 +0100

    Refactors evaluation code for threshold analysis
    Former-commit-id: 4b9ddd98fd8cfad03639f96f3d0c6af46495c5b5

---
+++ bash_scripts/model_threshold_search.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --output=model_threshold_search_%j.output
+export PYTHONPATH=/vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify:$PYTHONPATH
+export PATH=/vol/bitbucket/es1519/myvenv/bin/:$PATH
+source activate
+TERM=vt100 # or TERM=xterm
+/usr/bin/nvidia-smi
+uptime
+
+echo "Checkpoint: $1"
+
+python /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/model_eval/threshold_search.py --checkpoint $1
+
+# Param 1: checkpoint (e.g. /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/saved/Jigsaw_ALBERT_bias/lightning_logs/version_68502/checkpoints/epoch=0-step=60163.ckpt)
--- bash_scripts/old/evaluate_folder_old.sh
+++ bash_scripts/old/evaluate_folder_old.sh
@@ -10,6 +10,6 @@ uptime
 
 echo "Folder: $1"
 
-python /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/model_eval/evaluate_new.py --folder $1
+python /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/model_eval/evaluate_old.py --folder $1
 
 # Param 1: folder path (e.g. /vol/bitbucket/es1519/detecting-hidden-purpose-in-nlp-models/detoxify/saved/Jigsaw_ALBERT_bias/lightning_logs/version_68502/checkpoints/)
